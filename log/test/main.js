b7396322248b1afe2779c9649e56b1309a24e65f
Mark Cuypers <mark_cuypers@peopleware.be>
2013-12-05 17:16:15 +0100

    Unit tests for main.js updated to be at least 1 unit test per function in the real module.

--- log/test/main.js
+++ log/test/main.js
@@ -2,45 +2,36 @@ define(["doh/main", "log/main"], function (doh, logFactory) {
   doh.register("tests.log.main", [
     {
       // has["logLevel-test.log.main_INFO"] = "INFO";
-      name: "TEST main INFO",
+      name: "TEST[main] getInitialLogLevelFor",
       runTest: function (t) {
         var d = new doh.Deferred();
         var logger = logFactory.getLogger("test.log.main_INFO");
         setTimeout(d.getTestCallback(function () {
           t.assertEqual(logFactory.getInitialLogLevelFor(logger.name), "INFO");
-          t.assertEqual(logFactory.loggerName2Mid(logger.name), "test/log/main_INFO");
-          t.assertEqual(logFactory.mid2LoggerName("test/log/main_INFO"), logger.name);
-
         }), 500);
         return d;
       }
     },
     {
       // has["logLevel-test.log.main_DEBUG"] = "DEBUG";
-      name: "TEST main DEBUG",
+      name: "TEST[main] loggerName2Mid",
       runTest: function (t) {
         var d = new doh.Deferred();
         var logger = logFactory.getLogger("test.log.main_DEBUG");
         setTimeout(d.getTestCallback(function () {
-          t.assertEqual(logFactory.getInitialLogLevelFor(logger.name), "DEBUG");
           t.assertEqual(logFactory.loggerName2Mid(logger.name), "test/log/main_DEBUG");
-          t.assertEqual(logFactory.mid2LoggerName("test/log/main_DEBUG"), logger.name);
-
         }), 500);
         return d;
       }
     },
     {
       // has["logLevel-test.log.main_TRACE"] = "TRACE";
-      name: "TEST main TRACE",
+      name: "TEST[main] mid2LoggerName",
       runTest: function (t) {
         var d = new doh.Deferred();
         var logger = logFactory.getLogger("test.log.main_TRACE");
         setTimeout(d.getTestCallback(function () {
-          t.assertEqual(logFactory.getInitialLogLevelFor(logger.name), "TRACE");
-          t.assertEqual(logFactory.loggerName2Mid(logger.name), "test/log/main_TRACE");
           t.assertEqual(logFactory.mid2LoggerName("test/log/main_TRACE"), logger.name);
-
         }), 500);
         return d;
       }
